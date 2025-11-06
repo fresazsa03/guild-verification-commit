961c8db6690ac5eefe27e77bbd6562e6472dc08c
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:05:22 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:04:58.828Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T02:05:21.090Z
+Commit: Fix memory leak in event handlers
