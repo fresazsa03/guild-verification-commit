21a1ca35e049515136147eb241c03fe0fc7cdf4d
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 04:56:15 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:56:11.643Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:56:14.071Z
+Commit: Fix CSS styling bug on mobile devices
