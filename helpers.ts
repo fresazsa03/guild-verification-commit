1174eda76df44e35cb2ed47b12a7bbb84e39e647
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:00:51 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:00:33.044Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T02:00:50.538Z
+Commit: Correct typo in configuration file
