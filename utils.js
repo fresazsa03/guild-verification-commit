fc6a44efc935407038ba9301c0e02931778a901c
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 04:54:56 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:54:53.921Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T01:54:55.635Z
+Commit: Add multi-language support
