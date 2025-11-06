cf65d8c05c15a67c269b62131976c402faa22658
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:01:55 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:01:50.050Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:01:54.387Z
+Commit: Fix broken links in documentation
