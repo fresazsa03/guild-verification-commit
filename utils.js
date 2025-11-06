7112d7a626c52e304cd629490ff8214a34178fe2
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:06:13 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:06:01.650Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T02:06:12.028Z
+Commit: Fix CSS styling bug on mobile devices
