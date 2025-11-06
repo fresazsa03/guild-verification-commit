e10cc03297834d938a3751362d719d08768e4a59
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 04:56:12 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:55:57.163Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T01:56:11.643Z
+Commit: Fix broken links in documentation
