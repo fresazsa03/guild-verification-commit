5481165c5678d0c2f95c42642581e3e62386af99
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:01:09 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:00:59.807Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T02:01:08.134Z
+Commit: Fix broken links in documentation
