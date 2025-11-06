57c2a64668758c1fe486c762b26f8fe0b513a619
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:04:30 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:04:19.654Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T02:04:29.078Z
+Commit: Fix CSS styling bug on mobile devices
