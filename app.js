fab2b67e2814a70d4da309daa44b84d0b4f327a8
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:05:14 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:05:06.350Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T02:05:13.752Z
+Commit: Resolve null pointer exception in user service
