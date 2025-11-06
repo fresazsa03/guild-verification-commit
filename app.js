09f2d05bb36ced3547430dbc935d817a143bfbed
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 04:54:06 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:10:04.157Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T01:54:05.091Z
+Commit: Fix CSS styling bug on mobile devices
