7d94d2fba9cbf413cd36b93fe457fe1dda03e83a
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:04:28 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:01:22.403Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T02:04:27.835Z
+Commit: Resolve timezone issues in date handling
