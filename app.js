63c4f0904f1083b3d54f9217ef8367201e290295
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 04:54:44 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:54:19.287Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:54:43.037Z
+Commit: Fix CSS styling bug on mobile devices
