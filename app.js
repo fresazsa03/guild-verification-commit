c8695eb8fa63ef56b5687629be7c59a0305589e3
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 04:55:50 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:55:45.725Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T01:55:49.545Z
+Commit: Fix broken links in documentation
