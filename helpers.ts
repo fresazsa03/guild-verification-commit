237722bc762daa89cbc9a809df9e097f0c257075
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:00:34 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:56:14.071Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T02:00:33.044Z
+Commit: Fix broken links in documentation
