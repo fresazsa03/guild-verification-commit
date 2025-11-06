40b8b0d112a9674654798b9f9c0f7deb5a6cd734
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:04:50 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:04:45.847Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T02:04:49.610Z
+Commit: Introduce real-time notifications
