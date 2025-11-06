304e7bd03908183f38db02ff17c8562256d82344
potore <113534791+fresazsa03@users.noreply.github.com>
2025-11-06 05:01:18 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T02:01:06.845Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T02:01:17.363Z
+Commit: Fix CSS styling bug on mobile devices
